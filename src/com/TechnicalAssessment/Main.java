592df5b8036151a9fe4485ac3fc94f8294be0180
Rahulmodi-4468 <72561990+Rahulmodi-4468@users.noreply.github.com>
2021-08-18 00:15:31 +0530

    First Test Success Updated

--- src/com/TechnicalAssessment/Main.java
+++ src/com/TechnicalAssessment/Main.java
@@ -1,42 +1,60 @@
 package com.TechnicalAssessment;
 
-public class Main {
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class Main {
+    static  Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 	// write your code here
-        String num1 = "12,25";
-        String num2 = "";
-        String num3 = "5,15,25,36,9,8";
-        int val;
-        val= Add(num1);
-        System.out.println("-----------------------\nTest Case 1 :- \nUpto two numbers & their 'sum=' : "+ val);
-        val = Add(num2);
-        System.out.println("-----------------------\nTest Case 2 :-\nEmpty String : "+val);
-        val = Add(num3);
-        System.out.println("-----------------------\nTest Case 3 :-\nHandled Unknown Amount of Numbers & Display their 'Sum=' : "+val);
 
+        Main calc = new Main();
+        System.out.println("Enter Your String for testing");
+        String input = sc.nextLine();
+        int sum = calc.Add(input);
+        System.out.print(sum);
     }
     static int Add(String numbers)
     {
         int sum = 0;
-        String[] arr = numbers.split(",");
-        int len = arr.length;
-        if(numbers.length() == 0)
+        StringTokenizer tokens = new StringTokenizer(numbers,",");
+        int len = tokens.countTokens();
+        if(len <= 0)
         {
-            return sum;
+            System.out.println("-----------------------\nTest Case  :- \nEmpty String");
+            return 0;
         }
-        else if(len <= 2 )
-        {
-            int numArr[] = new int[len];
-
-            for (int i = 0; i < len; i++) {
-                numArr[i] = Integer.parseInt(arr[i]);
-                sum += numArr[i];
+        else if(len >= 1 && len <= 2){
+            while(tokens.hasMoreTokens()) {
+                sum += Integer.parseInt(tokens.nextToken());
             }
+            System.out.println("-----------------------\nTest Case  :- \nUpto two numbers & their 'sum=' : ");
 
             return sum;
         }
-        //else{
+        System.out.println("-----------------------\nTest Case  :- \nUnkown Amount of Numbers : " );
+        return 1;
+
+    }
+}
+
+
+/*
+* while(tokens.hasMoreTokens()) {
+            int val = Integer.parseInt(tokens.nextToken());
+            if (val <  0) {
+                throw new NegativeValueException("Negative Values Not allowed" + val);
+            } else if (len <= 2) {
+                int numArr[] = new int[len];
+
+                for (int i = 0; i < len; i++) {
+                    numArr[i] = Integer.parseInt(arr[i]);
+                    sum += numArr[i];
+                }
+
+                return sum;
+            }
+            //else{
             int numArr[] = new int[len];
             sum = 0;
             for (int i = 0; i < len; i++) {
@@ -44,8 +62,7 @@ public class Main {
                 sum += numArr[i];
             }
             //System.out.println("Handled Unknown Amount of numbers 'Sums =' : "+ sum);
-        //}
-        return sum;
-
-    }
-}
+            //}
+            return sum;
+        }
+* */
