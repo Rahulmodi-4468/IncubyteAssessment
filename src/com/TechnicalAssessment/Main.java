ea5e3e20cb24602fa0aed0d3703b107b29105ee5
Rahulmodi-4468 <72561990+Rahulmodi-4468@users.noreply.github.com>
2021-08-18 23:19:55 +0530

    Update test 1 & 2

--- src/com/TechnicalAssessment/Main.java
+++ src/com/TechnicalAssessment/Main.java
@@ -9,60 +9,42 @@ public class Main {
 	// write your code here
 
         Main calc = new Main();
-        System.out.println("Enter Your String for testing");
+        System.out.print("Enter Your String  : ");
         String input = sc.nextLine();
         int sum = calc.Add(input);
         System.out.print(sum);
     }
     static int Add(String numbers)
     {
-        int sum = 0;
-        StringTokenizer tokens = new StringTokenizer(numbers,",");
-        int len = tokens.countTokens();
-        if(len <= 0)
+        int sum = 0;                     //Initializing sum as 0
+        StringTokenizer tokens = new StringTokenizer(numbers,","); //Tokenizing String to Iterate
+        int len = tokens.countTokens();   //length of numbers
+        if(len == 0)                      //here Checking length is 0
         {
             System.out.println("-----------------------\nTest Case  :- \nEmpty String");
             return 0;
         }
-        else if(len >= 1 && len <= 2){
-            while(tokens.hasMoreTokens()) {
-                sum += Integer.parseInt(tokens.nextToken());
+        else if(len >= 1 && len <= 2){    //here checking length is upto 2
+            while(tokens.hasMoreTokens()) {   //Iterating token with hasMoreTokens() method of StringTokenizer
+                int num ;
+                try {
+                    num = Integer.parseInt(tokens.nextToken());
+                    sum += num;
+                }
+                catch(NumberFormatException ex)
+                {
+                    System.out.println("We can catch the NumberFormatException ");
+                }
             }
-            System.out.println("-----------------------\nTest Case  :- \nUpto two numbers & their 'sum=' : ");
+            System.out.print("-----------------------\nTest Case  :- \nUpto two numbers & their 'sum=' : ");
 
-            return sum;
+            return sum;                        //return Addition of numbers
         }
+
         System.out.println("-----------------------\nTest Case  :- \nUnkown Amount of Numbers : " );
         return 1;
 
     }
+
 }
 
-
-/*
-* while(tokens.hasMoreTokens()) {
-            int val = Integer.parseInt(tokens.nextToken());
-            if (val <  0) {
-                throw new NegativeValueException("Negative Values Not allowed" + val);
-            } else if (len <= 2) {
-                int numArr[] = new int[len];
-
-                for (int i = 0; i < len; i++) {
-                    numArr[i] = Integer.parseInt(arr[i]);
-                    sum += numArr[i];
-                }
-
-                return sum;
-            }
-            //else{
-            int numArr[] = new int[len];
-            sum = 0;
-            for (int i = 0; i < len; i++) {
-                numArr[i] = Integer.parseInt(arr[i]);
-                sum += numArr[i];
-            }
-            //System.out.println("Handled Unknown Amount of numbers 'Sums =' : "+ sum);
-            //}
-            return sum;
-        }
-* */
